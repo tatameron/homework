7b71d1bb8f860a3bf6ae3e748bea4e5b2b4dd935
tatameron <tatameron@gmail.com>
2018-06-16 12:54:17 +0000

    finished 5-8

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+def index
+	@posts = Post.all
+	@post = Post.new
+end
+
+def new
+	@post = Post.new
+end
+ def create
+        # ストロングパラメーターを使用
+         @post = Post.new(post_params)
+        # DBへ保存する
+         if @post.save
+         flash[:notice] = "Books was succesfully created."
+        redirect_to '/posts'
+    end
+    @posts = Post.all
+    render 'index'
+    end
+def show
+	@post = Post.find(params[:id])
+end
+
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	post = Post.find(params[:id])
+	if post.update(post_params)
+	flash[:notice] = "Books was succesfully updated."
+	redirect_to post_path(post)
+end
+end
+def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+    flash[:notice] = "Books was succesfully deleted."
+	redirect_to posts_path
+end
+end
+	private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
